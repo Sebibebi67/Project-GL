0079cade1f9e6337230797abd030b0008944b763
Sebibebi67 <sebibebi67@gmail.com>
2020-01-09 14:58:19 +0100

    import classes

---
+++ tools/Test.java
@@ -0,0 +1,11 @@
+package tools;
+
+/**
+ * 
+ * This class is composed by every test made in the conception
+ * @author Sébastien HERT (Group 8)
+ * 
+ */
+public class Test{
+
+}
---
+++ tools/Tool.java
@@ -0,0 +1,30 @@
+package tools;
+
+/**
+ * 
+ * This class is composed by usefull tools which simplify the code
+ * @author Sébastien HERT (Group 8)
+ * 
+ */
+
+public class Tool{
+
+    /**
+    * Print a String
+    * @author Sébastien HERT (group 8)
+    * @param String s to print
+    */
+    public static void print(String s){
+        System.out.println(s);
+    }
+
+    /**
+    * Print a Object
+    * @author Sébastien HERT (group 8)
+    * @param Object o to print
+    */
+    public static void display(Object o){
+        System.out.println(o.toString());
+    }
+
+}
