4a7e698550769bcaa5e038f832d0b5f9cbd8ba3c
Sebibebi67 <sebibebi67@gmail.com>
2020-01-12 14:52:49 +0100

    removing errors with comments

--- controllers/LoginController.java
+++ controllers/LoginController.java
@@ -1,28 +1,28 @@
-import java.net.URL;
-import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
+// import java.net.URL;
+// import java.util.ResourceBundle;
+// import javafx.event.ActionEvent;
+// import javafx.fxml.FXML;
+// import javafx.scene.control.Button;
 
-public class LoginController {
+// public class LoginController {
 
-    @FXML
-    private ResourceBundle resources;
+//     @FXML
+//     private ResourceBundle resources;
 
-    @FXML
-    private URL location;
+//     @FXML
+//     private URL location;
 
-    @FXML
-    private Button connexion;
+//     @FXML
+//     private Button connexion;
 
-    @FXML
-    void loginProgram(ActionEvent event) {
+//     @FXML
+//     void loginProgram(ActionEvent event) {
 
-    }
+//     }
 
-    @FXML
-    void initialize() {
-        assert connexion != null : "fx:id=\"connexion\" was not injected: check your FXML file 'login.fxml'.";
+//     @FXML
+//     void initialize() {
+//         assert connexion != null : "fx:id=\"connexion\" was not injected: check your FXML file 'login.fxml'.";
 
-    }
-}
+//     }
+// }
