f60b78efa4c4b5b0a756527a8659c62dd18b168c
Sebibebi67 <sebibebi67@gmail.com>
2020-01-12 14:49:54 +0100

    adding constructor for Professor class

--- user/Professor.java
+++ user/Professor.java
@@ -1,6 +1,48 @@
 package user;
 
+import java.util.ArrayList;
+
+import tools.Query;
+import study.Module;
+
 public class Professor implements Role{
 
+    private String login;
+    private ArrayList<Module> modules;
+
     public Professor(){}
+
+    public Professor(String login){
+        this.login = login;
+        this.createModule();
+    }
+
+    public void createModule(){
+        modules = new ArrayList<>();
+
+        ArrayList<String> array = new ArrayList<>();
+        array = Query.coursesTaught(this.login);
+
+        for (int i = 0 ; i < array.size() ; i++){
+            modules.add(new Module(array.get(i)));
+        }
+    }
+
+    public String getLogin() {
+        return this.login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public ArrayList<Module> getModules() {
+        return this.modules;
+    }
+
+    public void setModules(ArrayList<Module> modules) {
+        this.modules = modules;
+    }
+    
+
 }
