8697f657b00a5e182dcdbd5cbbf03cc8a558a998
Sebibebi67 <sebibebi67@gmail.com>
2020-01-18 17:15:10 +0100

    SO functions -> Administation

--- user/Administration.java
+++ user/Administration.java
@@ -1,8 +1,14 @@
 package user;
 
-public abstract class Administration implements Role{
+import java.util.ArrayList;
+
+import tools.Query;
+
+public abstract class Administration implements Role {
 
 	private String login;
+	ArrayList<ArrayList<String>> students;
+	ArrayList<String> modules;
 
 	public Administration () {
 
@@ -16,6 +22,45 @@ public abstract class Administration implements Role{
 	public void setLogin(String login)
 	{
 		this.login = login;
+	}
+	
+	public ArrayList<ArrayList<String>> getStudents() {
+		return this.students;
+	}
+
+	public void setStudents(ArrayList<ArrayList<String>> students) {
+		this.students = students;
+	}
+
+	public ArrayList<String> getModules() {
+		return this.modules;
+	}
+
+	public void setModules(ArrayList<String> modules) {
+		this.modules = modules;
+	}
+
+	public void initArray(){
+		students = new ArrayList<>();
+		modules = new ArrayList<>();
+	}
+
+	public void createStudents(){
+		ArrayList<ArrayList<String>> array = new ArrayList<>();
+		array = Query.studentsSO();
+
+		if (!array.get(0).isEmpty()){
+			for (int i = 0; i < array.get(0).size(); i++){
+				ArrayList<String> student = new ArrayList<>();
+				for (int j = 0; j<4; j++){
+					student.add(array.get(j).get(i));
+				}
+			}
+		}
+	}
+
+	public void createModules(){
+		modules = Query.modulesSO();
 	}
 
 }
