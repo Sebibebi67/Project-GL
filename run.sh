049465747c5f2c2aa9408a089d4601af41128db3
Sebibebi67 <sebibebi67@gmail.com>
2020-01-26 17:29:03 +0100

    update run.sh

--- run.sh
+++ run.sh
@@ -4,40 +4,14 @@
 #
 # It is supposed to allow everyone to compile the GL project on a Linux Distribution without problems
 
-	mainJava=tools/*.java
+	mainJava=*/*.java
 	main=tools.Main
-	controllersPackage=controllers
 
 	bool=0
     echo "Compilation in progress"
 	javac --module-path ./lib --add-modules javafx.controls,javafx.fxml $mainJava && bool=1
     if [[ $bool == 0 ]];then
         echo "Compilation failed"
-        exit 1
-    fi
-
-    javac --module-path ./lib --add-modules javafx.controls,javafx.fxml user/*.java && bool=1
-    if [[ $bool == 0 ]];then
-        echo "Compilation failed"
-        exit 1
-    fi
-
-    javac --module-path ./lib --add-modules javafx.controls,javafx.fxml admin/*.java && bool=1
-    if [[ $bool == 0 ]];then
-        echo "Compilation failed"
-        exit 1
-    fi
-
-    javac --module-path ./lib --add-modules javafx.controls,javafx.fxml study/*.java && bool=1
-    if [[ $bool == 0 ]];then
-        echo "Compilation failed"
-        exit 1
-    fi
-
-    echo "Update controllers"
-	javac --module-path ./lib --add-modules javafx.controls,javafx.fxml $controllersPackage/*.java && bool=1
-        if [[ $bool == 0 ]];then
-        echo "Update failed"
         exit 1
     fi
 
